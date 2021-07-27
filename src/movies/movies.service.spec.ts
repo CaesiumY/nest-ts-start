6d1e051f04ddbc183602e8a8b12a903c60667b5f
caesiumy <dbs2636@gmail.com>
2021-07-28 01:44:07 +0900

    test: Add Read testings

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -14,5 +15,31 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAllMovies', () => {
+    it('should return All Movies of Array', () => {
+      expect(service.getAllMovies()).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOneMovie', () => {
+    it('should return One Movie with ID', () => {
+      service.createMovie({
+        title: 'test',
+        year: 2021,
+        genres: ['test', 'test2'],
+      });
+
+      expect(service.getOneMovie(1)).toBeDefined();
+    });
+
+    it('should return an Error', () => {
+      try {
+        service.getOneMovie(9);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
